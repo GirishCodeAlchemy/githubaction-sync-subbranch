1a8274d1c9decac89a8368469d17f2c6a7b7fedb
GirishCodeAlchemy <venkatareddygirish@gmail.com>
2024-01-28 13:40:07 -0500

    Remove -v option in mail command

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -31,22 +31,6 @@ for branch in $(git branch -r | grep -v '\->'); do
     # Set output variable for conflict
     echo "::set-output name=conflict::$conflict"
 
-    # # Determine branch owner
-    # branch_owner=$(git log --format='%ae' -n 1 $branch_name)
-
-    # # Send email notification on merge conflict
-    # if [ -n "$conflict" ]; then
-    #   echo "Merge conflict in $conflict"
-
-    #   # Send email notification
-    #   echo "There was a merge conflict when syncing the branch $conflict with main." | mailx -s "Merge Conflict in $conflict" -r "$SMTP_USERNAME" -S smtp="smtp.gmail.com:465" -S smtp-use-ssl -S smtp-auth=login -S smtp-auth-user="$SMTP_USERNAME" -S smtp-auth-password="$SMTP_PASSWORD" $branch_owner
-    # else
-    #   echo "Merge successful in $branch_name"
-
-    #   # Send success email notification
-    #   echo "The branch $branch_name was successfully synced with main." | mailx -s "Merge Success in $branch_name" -r "$SMTP_USERNAME" -S smtp="smtp.gmail.com:465" -S smtp-use-ssl -S smtp-auth=login -S smtp-auth-user="$SMTP_USERNAME" -S smtp-auth-password="$SMTP_PASSWORD" $branch_owner
-    # fi
-
     # Determine branch owner
     branch_owner=$(git log --format='%ae' -n 1 $branch_name)
 
@@ -56,11 +40,11 @@ for branch in $(git branch -r | grep -v '\->'); do
       subject="Merge Conflict in $conflict"
 
       # Send email notification
-      echo "There was a merge conflict when syncing the branch $conflict with main." | mail -s "$subject" -v  $branch_owner
+      echo "There was a merge conflict when syncing the branch $conflict with main." | mail -s "$subject" $branch_owner
     else
       echo "Merge successful in $branch_name"
       subject="Merge Successful in $branch_name"
-      echo "The branch $branch_name was successfully synced with main." | mail -s "$subject" -v  $branch_owner
+      echo "The branch $branch_name was successfully synced with main." | mail -s "$subject" $branch_owner
     fi
   fi
 done
