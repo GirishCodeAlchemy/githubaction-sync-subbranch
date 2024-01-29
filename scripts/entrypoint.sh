01a8ee0acfe3d2fa061415d7695c497ecf253dda
GirishCodeAlchemy <venkatareddygirish@gmail.com>
2024-01-29 01:13:38 -0500

    Revert the entrypoint

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -11,62 +11,60 @@ git config --global push.default tracking
 
 git config --global --add safe.directory /github/workspace
 
-ls -la /github/workspace
-ls -la
-python /app/sync_branches.py
-# # Fetch all branches
-# git fetch --all
+# python /app/sync_branches.py
+# Fetch all branches
+git fetch --all
 
-# # Loop through all remote branches
-# for branch_name in $(git branch -r | grep -v '\->'); do
-#   # Extract local branch name from remote branch
-#   local_branch_name=$(echo $branch_name | sed 's/origin\///')
+# Loop through all remote branches
+for branch_name in $(git branch -r | grep -v '\->'); do
+  # Extract local branch name from remote branch
+  local_branch_name=$(echo $branch_name | sed 's/origin\///')
 
-#   if [ "$local_branch_name" != "main" ] && [ "$local_branch_name" != "master" ]; then
-#     echo "Syncing branch $local_branch_name"
+  if [ "$local_branch_name" != "main" ] && [ "$local_branch_name" != "master" ]; then
+    echo "Syncing branch $local_branch_name"
 
-#     # Check if branches are already in sync
-#     if git merge-base --is-ancestor main $branch_name; then
-#       echo "Branch $branch_name is already in sync with main. Nothing to merge."
-#       continue
-#     fi
+    # Check if branches are already in sync
+    if git merge-base --is-ancestor main $branch_name; then
+      echo "Branch $branch_name is already in sync with main. Nothing to merge."
+      continue
+    fi
 
-#     # Sync branches
-#     conflict=""
-#     git checkout main
-#     git fetch origin
-#     git switch $local_branch_name
-#     git pull
-#     echo "Syncing the changes from main to branch: $local_branch_name"
-#     git branch
-#     echo "get the status--->"
-#     git status
-#     git merge origin/main || conflict="$local_branch_name"
-#     echo "get the status--->"
-#     git status
-#     echo "get the diff--->"
-#     git diff
-#     git push origin $local_branch_name || conflict="$local_branch_name"
+    # Sync branches
+    conflict=""
+    git checkout main
+    git fetch origin
+    git switch $local_branch_name
+    git pull
+    echo "Syncing the changes from main to branch: $local_branch_name"
+    git branch
+    echo "get the status--->"
+    git status
+    git merge origin/main || conflict="$local_branch_name"
+    echo "get the status--->"
+    git status
+    echo "get the diff--->"
+    git diff
+    git push origin $local_branch_name || conflict="$local_branch_name"
 
-#     # Set output variable for conflict
-#     echo "::set-output name=conflict::$conflict"
+    # Set output variable for conflict
+    echo "::set-output name=conflict::$conflict"
 
-#     # Determine branch owner
-#     branch_owner=$(git log --format='%ae' -n 1 $local_branch_name)
+    # Determine branch owner
+    branch_owner=$(git log --format='%ae' -n 1 $local_branch_name)
 
-#     echo "Branch owner: $branch_owner"
+    echo "Branch owner: $branch_owner"
 
-#     # Send email notification on merge conflict
-#     if [ -n "$conflict" ]; then
-#       echo "Merge conflict in $conflict"
-#       # Uncomment the following lines when you are ready to send emails
-#       # subject="Merge Conflict in $conflict"
-#       # echo "There was a merge conflict when syncing the branch $conflict with main." | mail -s "$subject" $branch_owner
-#     else
-#       echo "Merge successful in $branch_name"
-#       # Uncomment the following lines when you are ready to send emails
-#       # subject="Merge Successful in $local_branch_name"
-#       # echo "The branch $local_branch_name was successfully synced with main." | mail -s "$subject" $branch_owner
-#     fi
-#   fi
-# done
+    # Send email notification on merge conflict
+    if [ -n "$conflict" ]; then
+      echo "Merge conflict in $conflict"
+      # Uncomment the following lines when you are ready to send emails
+      # subject="Merge Conflict in $conflict"
+      # echo "There was a merge conflict when syncing the branch $conflict with main." | mail -s "$subject" $branch_owner
+    else
+      echo "Merge successful in $branch_name"
+      # Uncomment the following lines when you are ready to send emails
+      # subject="Merge Successful in $local_branch_name"
+      # echo "The branch $local_branch_name was successfully synced with main." | mail -s "$subject" $branch_owner
+    fi
+  fi
+done
